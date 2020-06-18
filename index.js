f4abc26768b88317d50851d69b9f5438355f1c12
vozandrey <59666184+vozandrey@users.noreply.github.com>
2020-06-19 02:54:22 +0300

    delete process the home page of the site

--- index.js
+++ index.js
@@ -2,12 +2,6 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.status(200).json({
-        message: 'working'
-    });
-});
-
 app.listen(5000, () => {
     console.log('Server has been started');
 });
