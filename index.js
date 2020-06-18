12781223352cc1915fe73d95c5dbf4fab3c06211
vozandrey <59666184+vozandrey@users.noreply.github.com>
2020-06-19 02:53:37 +0300

    process the home page of the site

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (req, res) => {
+    res.status(200).json({
+        message: 'working'
+    });
+});
+
 app.listen(5000, () => {
     console.log('Server has been started');
 });
