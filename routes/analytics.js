c00c61a26074f2360724c04cc19592145d7b63e8
vozandrey <59666184+vozandrey@users.noreply.github.com>
2020-06-20 02:25:00 +0300

    add routes

--- routes/analytics.js
+++ routes/analytics.js
@@ -2,4 +2,7 @@ const express = require('express');
 const controller = require('../controllers/analytics');
 const router = express.Router();
 
+router.get('/overview', controller.overview);
+router.get('/analytics', controller.analytics);
+
 module.exports = router;
--- routes/category.js
+++ routes/category.js
@@ -2,4 +2,10 @@ const express = require('express');
 const controller = require('../controllers/category');
 const router = express.Router();
 
+router.get('/category', controller.getAll);
+router.get('/:id', controller.getById);
+router.delete('/:id', controller.remove);
+router.post('/', controller.create);
+router.patch('/:id', controller.update);
+
 module.exports = router;
