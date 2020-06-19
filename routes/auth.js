0f61ff20efb34e9244081135e44791b8f9096b20
vozandrey <59666184+vozandrey@users.noreply.github.com>
2020-06-19 22:38:50 +0300

    add routes for auth

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,11 @@
 const express = require('express');
+const controller = require('../controllers/auth');
 const router = express.Router();
 
+//localhost:5000/api/auth/login
+router.get('/login', controller.login);
+
+//localhost:5000/api/auth/login
+router.get('/register', controller.register);
 
 module.exports = router;
