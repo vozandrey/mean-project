a44bf1449dbba44eaeb7318a48358ed89d127570
vozandrey <59666184+vozandrey@users.noreply.github.com>
2020-06-20 02:19:16 +0300

    change method for auth routes

--- routes/auth.js
+++ routes/auth.js
@@ -3,9 +3,9 @@ const controller = require('../controllers/auth');
 const router = express.Router();
 
 //localhost:5000/api/auth/login
-router.get('/login', controller.login);
+router.post('/login', controller.login);
 
 //localhost:5000/api/auth/login
-router.get('/register', controller.register);
+router.post('/register', controller.register);
 
 module.exports = router;
