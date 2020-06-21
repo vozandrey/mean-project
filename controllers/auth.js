3c9eeaf000c02e5eda137b96cb5ae58a3d4d2b50
vozandrey <59666184+vozandrey@users.noreply.github.com>
2020-06-21 19:51:54 +0300

    check email for existence

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,5 @@
+const User = require('../models/User');
+
 module.exports.login = function(req, res) {
     res.status(200).json({
         login: {
@@ -7,8 +9,16 @@ module.exports.login = function(req, res) {
     });
 };
 
-module.exports.register = function(req, res) {
-    res.status(200).json({
-        login: 'register'
-    });
+module.exports.register = async function(req, res) {
+    // email, password
+    const candidate = await User.findOne({email: req.body.email});
+
+    if (candidate) {
+        // Пользователь существует, нужно отправить ошибку
+        res.status(409).json({
+            message: 'Такой email уже занят. Попробуйте другой.'
+        })
+    } else {
+        // Нужно создать пользователя
+    }
 }
