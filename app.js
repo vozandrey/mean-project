fe8016c449f7b92e2ddb321635b6dab9b490e518
vozandrey <59666184+vozandrey@users.noreply.github.com>
2020-06-21 17:58:47 +0300

    use cors

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const app = express();
 app.use(require('morgan')('dev'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.use(require('cors')());
 
 app.use('/api/auth', authRoutes);
 app.use('/api/analytic', analyticsRoutes);
