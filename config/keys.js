267adbeae8ffc90b2fe44ad30c30f6c263d73045
vozandrey <59666184+vozandrey@users.noreply.github.com>
2020-06-21 19:21:39 +0300

    create config file

---
+++ config/keys.js
@@ -0,0 +1,7 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://vozandrey:1122334@cluster0-gad55.mongodb.net/fullstack?retryWrites=true&w=majority',
+    mongoOptions: {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
+}
