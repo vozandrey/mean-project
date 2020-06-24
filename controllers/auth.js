92f214197289da6c21b9836fc86e9e7f1e59b27f
vozandrey <59666184+vozandrey@users.noreply.github.com>
2020-06-25 00:33:06 +0300

    add login function

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,13 +1,36 @@
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const User = require('../models/User');
+const keys = require('../config/keys');
 
-module.exports.login = function(req, res) {
-    res.status(200).json({
-        login: {
-            email: req.body.email,
-            password: req.body.password
+module.exports.login = async function(req, res) {
+    const candidate = await User.findOne({email: req.body.email});
+
+    if(candidate) {
+        //Проверка пароля, пользователь существует
+        const passwordResult = bcrypt.compareSync(req.body.password, candidate.password);
+        if(passwordResult) {
+            // Генерация токена, пароли совпали
+            const token = jwt.sign({
+                email: candidate.email,
+                userId: candidate._id
+            }, keys.jwt, {expiresIn: 3600});
+
+            res.status(200).json({
+                token: `Bearer ${token}`
+            }); 
+
+        } else {
+            res.status(401).json({
+                message: 'Пароли не совпадают. Попробуйте снова.'
+            });
         }
-    });
+    } else {
+        // Пользователя нет, ошибка
+        res.status(404).json({
+            message: 'Пользователь с таким email не найден'
+        });
+    }
 };
 
 module.exports.register = async function(req, res) {
